ecab1789b91f75acc7fc76d7fbb78da9b0c49287
Dustin Yang <srdpt3@gmail.com>
2023-07-16 20:18:27 -0400

    footer and navbar

--- car_showcase/app/page.tsx
+++ car_showcase/app/page.tsx
@@ -1,10 +1,23 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div>
+          <SearchBar />
+          <div>
+            {/* <CustomFilter title="fuel" />
+            <CustomFilter title="year" /> */}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ car_showcase/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchBar = () => {
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer></SearchManufacturer>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
