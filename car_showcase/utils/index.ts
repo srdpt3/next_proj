7bb91d5f121a33406f4836fc4d08c37b85b201df
Dustin Yang <srdpt3@gmail.com>
2023-07-18 23:53:16 -0400

    update

--- car_showcase/utils/index.ts
+++ car_showcase/utils/index.ts
@@ -1,25 +1,15 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'bc1ad6fdb5mshe40c48354e76c69p1f068ajsn6bf06ca92bf0',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
-};
-
-try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-} catch (error) {
-    console.error(error);
-}
-
-
 export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': 'bc1ad6fdb5mshe40c48354e76c69p1f068ajsn6bf06ca92bf0',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+
+        headers: headers,
+    });
+
+    const result = await response.json
+    return result;
 
 }
