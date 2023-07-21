a2841a2fd0d8baa825fef5c4c18e07bcf41fe03c
Dustin Yang <srdpt3@gmail.com>
2023-07-20 23:04:09 -0400

    displaying cars

--- car_showcase/app/page.tsx
+++ car_showcase/app/page.tsx
@@ -1,10 +1,11 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CustomFilter, Hero, SearchBar, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
 export default async function Home() {
   const allCars = await fetchCars();
-
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+  console.log(allCars);
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -20,6 +21,21 @@ export default async function Home() {
             <CustomFilter title="year" /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car}></CarCard>
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error_container">
+            <h2 className="text-blakc text-xl font-bold">Ooops, No results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
---
+++ car_showcase/components/CarCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium"> /day</span>
+      </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          //   width={50}
+          //   height={50}
+          alt="car model"
+          fill
+          priority
+          className="object-cotain"
+        ></Image>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
