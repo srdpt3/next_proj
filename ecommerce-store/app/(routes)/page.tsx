9e224d87d94544b2871305c4a01ef362b35c187a
Dustin Yang <srdpt3@gmail.com>
2023-08-22 21:56:30 -0400

    finished main product page

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/Product-list";
+export const revalidate = 0;
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+  const billboard = await getBillboard("fc33f0c6-fa9e-4b7c-b9a9-0d68fcb79d83");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -1,6 +1,29 @@
 "use client";
+
+import Button from "./ui/Button";
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 const NavbarActions = () => {
-  return <div className="ml-auto flex items-center gap-x-4">Hello Actions</div>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const router = useRouter();
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <div className="ml-auto flex items-center gap -x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white" />
+        <span className="ml-2 text-sm font-medium text-white">0</span>
+      </Button>
+    </div>
+  );
 };
 
 export default NavbarActions;
