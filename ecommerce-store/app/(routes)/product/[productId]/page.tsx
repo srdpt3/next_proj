ba32cee72154fd06e8fc57dcba554a0805244e6e
Dustin Yang <srdpt3@gmail.com>
2023-08-23 21:14:36 -0400

    update

---
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const revalidate = 0;
+
+interface ProductPageProps {
+  params: {
+    productId: string;
+  };
+}
+
+const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
+  const product = await getProduct(params.productId);
+
+  return <div>ProductPage</div>;
+};
+
+export default ProductPage;
