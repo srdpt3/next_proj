5bc7d6c85b533d2aeb2c253543a42a8762fada85
Dustin Yang <srdpt3@gmail.com>
2023-08-26 06:38:43 -0400

    udpate

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import getColors from "@/actions/get-colors";
 import React from "react";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
+import Filter from "./components/filter";
+import ProductCard from "@/components/ui/product.card";
 
 export const revalidate = 0;
 
@@ -36,7 +38,20 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
       <Container>
         <Billboard data={category.billboard}></Billboard>
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
-          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8"></div>
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes}></Filter>
+              <Filter valueKey="colorId" name="Colors" data={colors}></Filter>
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {/* {products.length === 0 && <NoResults />} */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
