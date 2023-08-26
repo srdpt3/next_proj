e9a5b291a67d08b5bfa802d9e765bd921472a483
Dustin Yang <srdpt3@gmail.com>
2023-08-25 22:48:42 -0400

    update

---
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,46 @@
+import getProducts from "@/actions/get-products";
+import getCategory from "@/actions/get-category";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+import React from "react";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0;
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(params.categoryId);
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard}></Billboard>
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8"></div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CategoryPage;
