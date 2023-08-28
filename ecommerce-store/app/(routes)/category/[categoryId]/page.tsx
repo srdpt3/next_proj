21eb9bf3489f0934f80ea5452afc68babf68f8af
Dustin Yang <srdpt3@gmail.com>
2023-08-27 21:44:17 -0400

    category finish

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import Filter from "./components/filter";
 import ProductCard from "@/components/ui/product.card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -39,6 +40,8 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard}></Billboard>
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilters sizes={sizes} colors={colors} />
+
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes}></Filter>
               <Filter valueKey="colorId" name="Colors" data={colors}></Filter>
