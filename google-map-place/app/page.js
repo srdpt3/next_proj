aa21c19ed740c776d088dedbd146967c8b74ef2d
Dustin Yang <srdpt3@gmail.com>
2023-09-06 23:23:43 -0400

    header

--- google-map-place/app/page.js
+++ google-map-place/app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
   useEffect(() => {
-    // if (!session.user) {
-    //   router.push("/Login");
-    // }
+    if (!session.user) {
+      router.push("/Login");
+    }
   }, [session]);
   return (
     <div>
