8327233fc0528b1cc52590f848818334f9f0c563
Dustin Yang <srdpt3@gmail.com>
2023-09-07 22:48:22 -0400

    category

--- google-map-place/app/page.js
+++ google-map-place/app/page.js
@@ -4,6 +4,7 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import CategoryList from "./components/Home/CategoryList";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -14,9 +15,13 @@ export default function Home() {
     }
   }, [session]);
   return (
-    <div>
-      <h2>asdfasdf</h2>
-      <button onClick={() => signOut()}>SignOut</button>
+    <div className="grid grid-cols-4 h-screen">
+      <div className="p-3">
+        <CategoryList />
+      </div>
+      <div className="col-span-3">Second</div>
+
+      {/* <button onClick={() => signOut()}>SignOut</button> */}
     </div>
   );
 }
